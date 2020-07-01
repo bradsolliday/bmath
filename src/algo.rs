6890c1bee5504a4aaf8b8ff3c070a0a54b91b382
Bradley-Solliday <bradley@DESKTOP-3JLURJ2.localdomain>
2020-07-01 11:57:38 -0400

    Create skeleton of gcd_factors and add unit test.

--- src/algo.rs
+++ src/algo.rs
@@ -16,21 +16,43 @@ pub fn gcd(mut m: u64, mut n: u64) -> u64 {
     }
 }
 
+pub fn gcd_factors(m: u64, n: u64) -> (u64, u64, u64) {
+    panic!("Not implemented yet");
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::cmp;
+
+    // Returns nothing
+    // Panics if d is not the gcd of m and n
+    fn is_gcd(d: u64, m: u64, n: u64) {
+        assert_eq!(m % d, 0);
+        assert_eq!(n % d, 0);
+        for i in (d + 1)..cmp::min(n, m) {
+            assert!(m % i != 0 || n % i != 0);
+        }
+    }
 
     #[test]
     fn test_gcd() {
         for m in 1..200 {
             for n in 1..200 {
                 let d = gcd(m, n);
-                assert_eq!(m % d, 0);
-                assert_eq!(n % d, 0);
-                for j in (d + 1)..cmp::min(n, m) {
-                    assert!(m % j != 0 || n % j != 0);
-                }
+                is_gcd(d, m, n);
+            }
+        }
+    }
+
+    #[test]
+    fn test_gcd_factors() {
+        for m in 1..200 {
+            for n in 1..200 {
+                let (d, a, b) = gcd_factors(m, n);
+                assert_eq!(a*m + b*n, d);
+                is_gcd(d, m, n);
             }
         }
     }
