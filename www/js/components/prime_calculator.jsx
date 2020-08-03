fa0dd64ddd88a0d7ce17b5e86d27e74a00a75522
Bradley-Solliday <bradley@DESKTOP-3JLURJ2.localdomain>
2020-08-02 22:07:39 -0400

    Fix overflow bug introduced in last commit

--- www/js/components/prime_calculator.jsx
+++ www/js/components/prime_calculator.jsx
@@ -24,7 +24,7 @@ export class PrimeCalculator extends React.Component {
         this.state = {
             input_n: 1,
             n: 1, // n as in nth prime
-            cache: PCache.new(10000),
+            cache: PCache.new(1000),
             nth_prime: 2,
             auto_calculate: true
         }
@@ -46,8 +46,8 @@ export class PrimeCalculator extends React.Component {
         if (new_n <= 0) {
             new_n = 1;
         }
-        if (new_n > 6350) {
-            new_n = 6350;
+        if (new_n > 6300) {
+            new_n = 6300;
         }
         if (this.state.auto_calculate) {
             this.setState({
