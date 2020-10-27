755158042d9bd1e36deba9c7c36fd4d9d466c8f1
Bradley-Solliday <bradley@DESKTOP-3JLURJ2.localdomain>
2020-10-27 02:02:14 -0400

    Move construction of force vector into own func
    (Refactoring)

--- finite_difference/finite_difference_1.cpp
+++ finite_difference/finite_difference_1.cpp
@@ -15,6 +15,17 @@ double f(double x) {
     return M_PI_2 * M_PI_2 * std::cos(M_PI_2 * x);
 }
 
+// Returns a sparse Top matrix (second difference matrix with
+// u'' = 0 boundary condition on the top and u = 0 boundary condition
+// on the bottom, where u is the function we are differentiating. The
+// matrix is off the form)
+// [  1 -1  0  0  0 ]
+// [ -1  2 -1  0  0 ]
+// [  0 -1  2 -1  0 ]
+// [  0  0 -1  2 -1 ]
+// [  0  0  0 -1  2 ]
+// 
+// Precondition: dimension >= 2
 Eigen::SparseMatrix<double>
 top_matrix(int dimension) {
     Eigen::SparseMatrix<double> out(dimension, dimension);
@@ -31,6 +42,25 @@ top_matrix(int dimension) {
     return out;
 }
 
+// Returns the vectorized form of the function f on the discretized interval
+// starting at start with size steps of step_size length.
+// f is meant to represent the force being applied in the differential equation
+// we're solving.
+// If you didn't define this function, the function f is almost certainly not
+// the one you want and you'd be better off writing your own version.
+//
+// Precondition: size > 0
+Eigen::VectorXd
+force_vector(double start, double step_size, int size) {
+    Eigen::VectorXd out = Eigen::VectorXd::Zero(size);
+    double x = start;
+    for (int i = 0; i < size; ++i) {
+        out[i] = f(x);
+        x += step_size;
+    }
+    return out;
+}
+
 int main() {
     using namespace Eigen;
     const int n = 100; // The number of points we evaluate at
@@ -38,16 +68,11 @@ int main() {
     const double end = 1;
 
     // Builging the top matrix T
-    SparseMatrix<double> T = top_matrix(n);
+    const SparseMatrix<double> T = top_matrix(n);
 
     // Initializing the force vector F
     const double h = (end - start) / (n - 1.0);
-    VectorXd F = VectorXd::Zero(n);
-    double x = start;
-    for (int i = 0; i < n; ++i) {
-        F[i]= f(x);
-        x += h;
-    }
+    const VectorXd F = force_vector(start, h, n);
 
     // Solving
     SimplicialLLT<SparseMatrix<double>> solver;
@@ -65,7 +90,7 @@ int main() {
 
     std::vector<double> xpoints(n);
     std::vector<double> exact_soln(n);
-    x = start;
+    double x = start;
     for (int i = 0; i < n; ++i) {
         xpoints[i] = x;
         exact_soln[i] = u(x);
