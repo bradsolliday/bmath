691f4f28e48daaafb807e28483316ed5573d397b
bradsolliday <bms284@cornell.edu>
2021-03-31 20:40:08 -0700

    Add page for distances between line segments

--- notebooks/gen_html_of_invert_function.sh
+++
@@ -1,1 +0,0 @@
-jupyter nbconvert invert_function.ipynb --to html --TagRemovePreprocessor.remove_input_tags='{"remove_input"}' --TagRemovePreprocessor.remove_cell_tags='{"remove"}'
---
+++ notebooks/ipynb_to_html.sh
@@ -0,0 +1,1 @@
+jupyter nbconvert $1 --to html --TagRemovePreprocessor.remove_input_tags='{"remove_input"}' --TagRemovePreprocessor.remove_cell_tags='{"remove"}'
